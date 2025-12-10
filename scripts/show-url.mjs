acc3245b9a3eecfbbd3b5467a9d9baeb0cc7f888
brikeld <129516277+brikeld@users.noreply.github.com>
2025-12-10 15:50:45 +0100

    après consulte 10-12
    ça va pas mal

---
+++ scripts/show-url.mjs
@@ -0,0 +1,23 @@
+import os from 'os';
+
+const interfaces = os.networkInterfaces();
+
+let lanIp = null;
+for (const name of Object.keys(interfaces)) {
+  for (const info of interfaces[name] || []) {
+    if (info.family === 'IPv4' && !info.internal) {
+      lanIp = info.address;
+      break;
+    }
+  }
+  if (lanIp) break;
+}
+
+if (!lanIp) {
+  console.log('Could not determine local network IP.');
+} else {
+  const port = process.env.PORT || 5173;
+  console.log(`Network link: http://${lanIp}:${port}`);
+}
+
+
